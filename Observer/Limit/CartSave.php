c5334415308b761af390bc1827cc3eac22552a31
Bruno Roeder <bruno@trezo.com.br>
2017-06-06 13:53:57 -0300

    Adicionado modulo Trezo_LimitCartItemQty

---
+++ Observer/Limit/CartSave.php
@@ -0,0 +1,76 @@
+<?php
+/**
+* Trezo
+*
+* NOTICE OF LICENSE
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade Magento to newer
+* versions in the future. If you wish to customize Magento for your
+* needs please refer to http://www.bseller.com.br for more information.
+*
+* @category Trezo
+* @package Trezo_LimitCartItemQty
+*
+* @copyright Copyright (c) 2017 Trezo. (http://www.trezo.com.br)
+*
+* @author Bruno Roeder <bruno@trezo.com.br>
+*/
+
+namespace Trezo\LimitCartItemQty\Observer\Limit;
+
+class CartSave implements \Magento\Framework\Event\ObserverInterface
+{
+    protected $_checkoutSession;
+    protected $_scopeConfig;
+    protected $_logger;
+    protected $_messageManager;
+    protected $_message = null;
+    protected $_totalQtyInCart;
+
+    public function __construct(
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Message\ManagerInterface $messageManager
+    ) {
+        $this->_scopeConfig         = $scopeConfig;
+        $this->_checkoutSession     = $checkoutSession;
+        $this->_logger              = $logger;
+        $this->_messageManager      = $messageManager;
+    }
+
+    /**
+     * Execute observer, limiting the qty of intens in cart
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function execute(
+        \Magento\Framework\Event\Observer $observer
+    ) {
+        if ($this->_scopeConfig->getValue(
+            'limitcartitemqty/configuration/enabled',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        )) {
+            $object      = $observer->getObject();
+            // only cart item
+            if ($object instanceof \Magento\Quote\Model\Quote\Item) {
+                $totalQtyInCart = $object->getQuote()->getItemsQty();
+                $maxItemsQtyInCart = $this->_scopeConfig->getValue(
+                    'limitcartitemqty/configuration/max_item_qty',
+                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+                );
+
+                if ($totalQtyInCart > $maxItemsQtyInCart) {
+                    $erroMessage = $this->_scopeConfig->getValue(
+                        'limitcartitemqty/configuration/error_message',
+                        \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+                    );
+                    throw new \Magento\Framework\Exception\LocalizedException(__(sprintf($erroMessage, $maxItemsQtyInCart)));
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# module-limitcartitemqty
-Magento 2 modulo to limit the qty of itens in the cart.
+# Extensão Limitar Quantidade de Itens no Carrinho para Magento 2 (Magento CE 2)
+Com este módulo é possível limitar a quantidade de itens que seu cliente possui no carrinho em cada compra, possibilitando que se limite uma quantidade personalizada e que quando o limite seja atingido, uma mensagem informativa é apresentada ao seu cliente (mensagem personalizada via Admin).
+## Instalação
+### Instalar usando o [composer](https://getcomposer.org/):
+
+1. Entre na pasta raíz da sua instalação
+2. Digite o seguinte comando:
+    ```bash
+    composer require mageup/module-limitcartitemqty:dev-master
+    ```
+
+3. Digite os seguintes comandos, no terminal, para habilitar o módulo:
+
+    ```bash
+    php bin/magento module:enable Trezo_LimitCartItemQty --clear-static-content
+    php bin/magento setup:upgrade
+    ```
+### ou baixar e instalar manualmente:
+
+
+* Criar a seguinte estrutura de pastas app/code/Trezo/LimitCartItemQty
+* Baixe a ultima versão [aqui](https://codeload.github.com/mageup/module-limitcartitemqty/zip/master)
+* Descompacte o arquivo baixado e copie as pastas para dentro do diretório criado no início
+* Digite os seguintes comandos, no terminal, para habilitar o módulo:
+
+    ```bash
+    php bin/magento module:enable Trezo_LimitCartItemQty --clear-static-content
+    php bin/magento setup:upgrade
+    ```
